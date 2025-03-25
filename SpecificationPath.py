671a5884e322ce25af91e084d2a9ad5d6fd31bb6
Sambroge <92974992+Sambroge@users.noreply.github.com>
2025-03-25 17:06:03 +0300

    Обновил состав

--- SpecificationPath.py
+++
@@ -1,1 +0,0 @@
-path = r"C:\Users\Tarnogradskij\AppData\Local\Top Systems\T-FLEX CAD 3D 18\Rus\ProductStructs\Спецификация GEOM.xml"
--- main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    import GuidFinder
-    import Transletirate
-
-
-if __name__ == "__main__":
-    main()
