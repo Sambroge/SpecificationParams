0be533ade5368258187a8686ffde202f7577dd13
Sambroge <92974992+Sambroge@users.noreply.github.com>
2025-03-25 17:00:22 +0300

    Обновил файлы

--- GuidFinder.py
+++ GuidFinder.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 from ParameterNames import array_to_find
+from SpecificationPath import path
 
 max_len = max(map(len, array_to_find))
-#Путь, в функции "open" ниже, нужно поменять на свой.
-with open(r"C:\Users\Tarnogradskij\AppData\Local\Top Systems\T-FLEX CAD 3D 18\Rus\ProductStructs\Спецификация GEOM.xml", 'r', encoding='utf-8') as file:
+#Путь, в функции "open" ниже, нужно поменять на свой. Путь до файла спецификации формата .xml
+with open(path, 'r', encoding='utf-8') as file:
     soup = BeautifulSoup(file.read(), features='xml')
     with open('ResultGuids.txt', 'w', encoding='UTF-8') as result_file:
         for i in soup.Parameters.find_all("Parameter"):
---
+++ SpecificationPath.py
@@ -0,0 +1,1 @@
+path = r"C:\Users\Tarnogradskij\AppData\Local\Top Systems\T-FLEX CAD 3D 18\Rus\ProductStructs\Спецификация GEOM.xml"
